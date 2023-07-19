648d847fbb8c472379744209e9e88735ece8d8e9
hillarioncodes <lansoyin@gmail.com>
2023-07-19 09:20:37 -0700

    123

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,15 @@
-#includes "main.h"
+#include "main.h"
 /**
- *  TASK NUMER TWO
+ * print_alphabet_x10.c
  *
- *
+ * Return 0
  */
-
 void print_alphabet_x10(void)
 {
 	int i;
 	int j;
 
-	for(j = 1; i <= 10; i++)
+	for(i = 1; i <= 10; i++)
 	{
 		for(j = 97; j <= 122; j++)
 		{
