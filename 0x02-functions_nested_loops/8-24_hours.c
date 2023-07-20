9d72d7001664e2859a33c86743b84e6eeb834699
hillarioncodes <lansoyin@gmail.com>
2023-07-19 18:49:03 -0700

    7 et 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,10 @@
 void jack_bauer(void)
 {
 	int a, b, c, d;
-	for (a = 0; b <= 2; a++)
+
+	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; c <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			for (c = 0; c <= 5; c++)
 			{
